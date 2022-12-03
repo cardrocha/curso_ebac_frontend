8933451f71abe36eb3c7b8612ff8811c4001ed94
Ricardo Rocha <cardrocharj@gmail.com>
2022-12-03 16:29:01 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000'),
+    $('#cpf').mask('000.000.000-00'),
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            }
+        },
+        messages: {
+        nome: 'Por favor, insira o seu nome',
+        email: 'Digite seu email, obrigatório',
+        telefone: 'Digite seu telefone, obrigatório',
+        cpf: 'Digite seu CPF, obrigatório',
+        endereco: 'Digite seu endereço, obrigatório',
+        cep: 'Digite seu CEP, obrigatório'
+        }
+    })
+})
