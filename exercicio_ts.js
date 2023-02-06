3b39e449d26ac4630a70304e1c567d9bc29375a1
Ricardo Rocha <cardrocharj@gmail.com>
2023-02-05 21:59:56 -0300

    fix compilação para o js

--- exercicio_ts.js
+++ exercicio_ts.js
@@ -1,7 +1,10 @@
 "use strict";
-// function multiplicar(numeroUm: number, numeroDois: number): number {
-//     return numeroUm * numeroDois;
-// }
+function multiplicar(numeroUm, numeroDois) {
+    return numeroUm * numeroDois;
+}
 function saudacao(nome) {
     return `Olá + ${nome}`;
+}
+function saudacao2(nome) {
+    return "Olá" + nome;
 }
